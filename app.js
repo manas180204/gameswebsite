83b84aa002fecaf743662a492c2a78c887d0913e
manas180204 <manasgupta0715@gmail.com>
2024-12-08 18:45:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// When the Sudoku button is clicked, open the Sudoku game
+document.getElementById('playSudoku').addEventListener('click', function() {
+    window.open('../games/SudokuGame.jar', '_blank');
+});
+
+// When the Snake button is clicked, open the Snake game
+document.getElementById('playSnake').addEventListener('click', function() {
+    window.open('../games/SnakeGame.jar', '_blank');
+});
